f8fe8504eee6b412fa47c2c7d7e8b9127e41b5e2
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2025-03-22 22:36:33 +0300

    Calculate sum

--- cli-with-logging-client/src/main/java/acme/App.java
+++ cli-with-logging-client/src/main/java/acme/App.java
@@ -3,17 +3,47 @@ package acme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.LocalDateTime;
+
 /**
  * Hello world!
  */
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
 
+    private static int getHours() {
+        int hours = LocalDateTime.now().getHour();
+
+        log.info("Step 1: hours={}", hours);
+
+        return hours;
+    }
+
+    private static int getMinutes() {
+        int minutes = LocalDateTime.now().getMinute();
+
+        log.info("Step 2: minutes={}", minutes);
+
+        return minutes;
+    }
+
+    private static int getSeconds() {
+        int seconds = LocalDateTime.now().getSecond();
+
+        log.info("Step 3: seconds={}", seconds);
+
+        return seconds;
+    }
+
     public static void main(String[] args) {
-        log.info("Start...");
+        log.info("Starting...");
 
-        System.out.println("Hello World!");
+        int sum = 0;
 
-        log.info("Done");
+        sum += getHours();
+        sum += getMinutes();
+        sum += getSeconds();
+
+        log.info("Done: sum={}", sum);
     }
 }
